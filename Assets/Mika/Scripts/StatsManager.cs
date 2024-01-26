53a9eeff78cd12dfe575e8548d3e9c97c16a9a1c
MikaelaAbrilDz <mikaelaabrildiazvlc@gmail.com>
2024-01-26 23:36:47 +0100

    cambio de categoría implementado con cooldown

--- Assets/Mika/Scripts/StatsManager.cs
+++ Assets/Mika/Scripts/StatsManager.cs
@@ -6,6 +6,8 @@ public class StatsManager : MonoBehaviour
 {
     int viewers;
     [SerializeField] float streamQuality;
+    public float categoryCoolDown;
+    [SerializeField] string currentCategory = "null";
 
     private void Start()
     {
@@ -14,25 +16,42 @@ public class StatsManager : MonoBehaviour
     private void Update()
     {
         print(viewers);
+        categoryCoolDown = categoryCoolDown - 1 * Time.deltaTime;
     }
     public void PlusEuros()
     {
-        if (streamQuality >= 41 && streamQuality <= 60)
+        if (streamQuality <= 20 && categoryCoolDown > 3)
         {
             viewers += 1;
         }
-        if (streamQuality >= 61 && streamQuality <= 80)
+        if (streamQuality >= 21 && streamQuality <= 40 && categoryCoolDown > 3)
         {
             viewers += 2;
         }
-        if (streamQuality >= 81)
+        if (streamQuality >= 41 && streamQuality <= 60 && categoryCoolDown > 3)
         {
-            viewers += 3;
+            viewers += 4;
+        }
+        if (streamQuality >= 61 && streamQuality <= 80 && categoryCoolDown > 3)
+        {
+            viewers += 8;
+        }
+        if (streamQuality >= 81 && categoryCoolDown > 3)
+        {
+            viewers += 16;
+        }
+    }
+    public void ChangeCategory(string category)
+    {
+        if (category != currentCategory)
+        {
+            currentCategory = category;
+            categoryCoolDown = 10;
         }
     }
     private void DownViewers()
     {
-        if (streamQuality <= 20)
+        if (categoryCoolDown <= 0)
         {
             viewers -= 5;
         }
