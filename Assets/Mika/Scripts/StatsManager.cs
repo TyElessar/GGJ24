2ae6fe38b06e81994fcfe58ad64f5a78b9fe9fdc
MikaelaAbrilDz <mikaelaabrildiazvlc@gmail.com>
2024-01-27 13:16:30 +0100

    donaciones mejoradas

--- Assets/Mika/Scripts/StatsManager.cs
+++ Assets/Mika/Scripts/StatsManager.cs
@@ -10,6 +10,10 @@ public class StatsManager : MonoBehaviour
     bool flat, crypto;
     int viewersToAdd = 1;
 
+    [SerializeField] GameObject[] donationNotifs;
+    bool don20_0 = false, don20_1 = false, don50_0 = false, don50_1 = false, don50_2 = false, don100_0 = false, don100_1 = false, don100_2 = false,
+     don1000_0 = false, don1000_1 = false, don1000_2 = false, don1000_3 = false, don1000_4 = false;
+
     [SerializeField] TextMeshProUGUI moneyText;
     [SerializeField] Button[] updatesButtons;
 
@@ -34,6 +38,7 @@ public class StatsManager : MonoBehaviour
         ChangeCategory("JustChatting");
         InvokeRepeating("DownViewers", 10, 2);
         InvokeRepeating("UpMoney", 1, 1);
+        InvokeRepeating("GenerateDonations", 1, 1);
     }
     private void Update()
     {
@@ -113,11 +118,11 @@ public class StatsManager : MonoBehaviour
         {
             viewersToAdd += 32;
         }
-        if (whichAdd == "Girlfiend")
+        if (whichAdd == "girlfiend")
         {
             viewersToAdd += 64;
         }
-        if (whichAdd == "Bots")
+        if (whichAdd == "bots")
         {
             viewersToAdd += 128;
         }
@@ -144,6 +149,78 @@ public class StatsManager : MonoBehaviour
             flat = true;
         }
 
+    }
+    public void Donation(int donation)
+    {
+        money = money + donation;
+    }
+    public void Donation2(string donation)
+    {
+        if (donation == "20_0")
+        {
+            don20_0 = false;
+            donationNotifs[0].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "20_1")
+        {
+            don20_1 = false;
+            donationNotifs[1].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "50_0")
+        {
+            don50_0 = false;
+            donationNotifs[2].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "50_1")
+        {
+            don50_1 = false;
+            donationNotifs[3].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "50_2")
+        {
+            don50_2 = false;
+            donationNotifs[4].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "100_0")
+        {
+            don100_0 = false;
+            donationNotifs[5].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "100_1")
+        {
+            don100_1 = false;
+            donationNotifs[6].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "100_2")
+        {
+            don100_2 = false;
+            donationNotifs[7].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "1000_0")
+        {
+            don1000_0 = false;
+            donationNotifs[8].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "1000_1")
+        {
+            don1000_1 = false;
+            donationNotifs[9].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "1000_2")
+        {
+            don1000_2 = false;
+            donationNotifs[10].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "1000_3")
+        {
+            don1000_3 = false;
+            donationNotifs[11].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
+        if (donation == "1000_4")
+        {
+            don1000_4 = false;
+            donationNotifs[12].transform.position = new Vector2(Screen.width * 5, Screen.height * 5);
+        }
     }
     public void AddStrike()
     {
@@ -427,6 +504,95 @@ public class StatsManager : MonoBehaviour
         if (categoryCoolDown > 0)
         {
             coolDownToZeroNoticed = false;
+        }
+    }
+    private void GenerateDonations()
+    {
+        int donationRatio = 10;
+        if (Random.Range(0, donationRatio) == 0)
+        {
+
+            if (viewers < 250)
+            {
+                if (!don20_0)
+                {
+                    don20_0 = true;
+                    donationNotifs[0].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+                else if (!don20_1)
+                {
+                    don20_1 = true;
+                    donationNotifs[1].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+            }
+            if (viewers > 251 && viewers < 1000)
+            {
+                donationRatio = 8;
+                if (!don50_0)
+                {
+                    don50_0 = true;
+                    donationNotifs[2].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+                else if (!don50_1)
+                {
+                    don50_1 = true;
+                    donationNotifs[3].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+                else if (!don50_2)
+                {
+                    don50_2 = true;
+                    donationNotifs[4].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+            }
+            if (viewers > 1001 && viewers < 10000)
+            {
+                donationRatio = 4;
+                if (!don100_0)
+                {
+                    don100_0 = true; 
+                    donationNotifs[5].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+                else if (!don100_1)
+                {
+                    don100_1 = true;
+                    donationNotifs[6].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+
+                }
+                else if (!don100_2)
+                {
+                    don100_2 = true;
+                    donationNotifs[7].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+            }
+            if (viewers > 10001)
+            {
+                donationRatio = 3;
+                if (!don1000_0)
+                {
+                    don1000_0 = true;
+                    donationNotifs[8].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+                else if (!don1000_1)
+                {
+                    don1000_1 = true;
+                    donationNotifs[9].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+                else if (!don1000_2)
+                {
+                    don1000_2 = true;
+                    donationNotifs[10].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+                else if (!don1000_3)
+                {
+                    don1000_3 = true;
+                    donationNotifs[11].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+                else if (!don1000_4)
+                {
+                    don1000_4 = true;
+                    donationNotifs[12].transform.position = new Vector2(Random.Range(Screen.width / 5, Screen.width / 5 * 4), Random.Range(Screen.height / 5, Screen.height / 5 * 4));
+                }
+            }
         }
     }
     public void QuitCoolDownNotification()
